fb93d921688c2a4098830565c889867bd219b380
Cassio Luciano da silva <cassiolucianodasilva@gmail.com>
2024-04-28 23:05:47 -0300

    Atualização conteudo aula 2

---
+++ WEB/2ºBimestre/1 - integração HTML + css js III/script.js
@@ -0,0 +1,13 @@
+function filterImages(category) {
+    const images = document.querySelectorAll('.image');
+    images.forEach(image => {
+      if (category === 'all') {
+        image.style.display = 'block';
+      } else if (image.classList.contains(category)) {
+        image.style.display = 'block';
+      } else {
+        image.style.display = 'none';
+      }
+    });
+  }
+  
